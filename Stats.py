43483b8909e58e7dbd7a0672f816223e2d117ff0
Ulrich Thiel <thiel@mathematik.uni-stuttgart.de>
2016-09-22 13:38:31 +0200

    Analysis works

--- Stats.py
+++ Stats.py
@@ -111,146 +111,97 @@ if years is not None:
 	
 	
 ##############################################################################
-def Analyze(sensor, datehours, verbose=None):
-	
-	measurand = ((dbcursor.execute("SELECT Measurand From Sensors WHERE ID IS " + str(sensor))).fetchone())[0]
-	unit = ((dbcursor.execute("SELECT Unit From Sensors WHERE ID IS " + str(sensor))).fetchone())[0]
-	calibration = ((dbcursor.execute("SELECT Calibration From Sensors WHERE ID IS " + str(sensor))).fetchone())[0]	
-
-	#read data	
-	if verbose is None:	
-		verbose = False
-	
-	if verbose:
-		widgets = [
-			'Reading data:  ', Percentage(),
-			' ', Bar(),
-			' ', ETA()
-		]
-		pbar = ProgressBar(widgets=widgets, maxval=len(datehours), CR=True)
-		pbar.start()
-			
-	#read data
-	data = dict()
-	availabledatehours = []	#tuples [y,m,d,h] so that at least one data point exists for this datehour
-	availabledates = []
-	count = 0	
-	for e in datehours:
-		y = e[0]
-		m = e[1]
-		d = e[2]
-		h = e[3]
-		starttimestamp = TimestampOfDatetime(str(y)+"-"+str(m)+"-"+str(d)+" "+str(h)+":00")
-		stoptimestamp = TimestampOfDatetime(str(y)+"-"+str(m)+"-"+str(d)+" "+str(h)+":59")+60.0
-		res = (dbcursor.execute("SELECT Timestamp, Value FROM Data WHERE Sensor IS "+str(sensor)+' AND Timestamp>='+str(starttimestamp)+' AND Timestamp<'+str(stoptimestamp)+' ORDER BY Timestamp ASC').fetchall())
-		if len(res) > 0:
-			if y not in data.keys():
-				data[y] = dict()
-			if m not in data[y].keys():
-				data[y][m] = dict()
-			if d not in data[y][m].keys():
-				data[y][m][d] = dict()
-			if [y,m,d,h] not in availabledatehours:
-				availabledatehours.append([y,m,d,h])
-			if [y,m,d] not in availabledates:
-				availabledates.append([y,m,d])
-			calibres = [ (r[0],r[1]+calibration) for r in res ]	
-			data[y][m][d][h] = numpy.array(calibres, dtype=[('timestamp', numpy.uint32),('value',numpy.float64)])	
-			
-		count = count + 1		
-		if verbose:
-			pbar.update(count)
-		
-	if verbose:	
-		pbar.finish()
-		sys.stdout.write("\033[K") # 
+def Analyze(sensor, data):
 		
 	#total data
-	totaldata = numpy.concatenate([data[e[0]][e[1]][e[2]][e[3]] for e in availabledatehours])
+	totaldata = numpy.concatenate([data[d] for d in data.keys()])
 	
 	#total maximum
 	totalmax = totaldata['value'].max()	
-	totalmaxdatehours = [DateAndHourFromTimestamp(totaldata['timestamp'][i]) for i in (numpy.argwhere(totaldata['value'] == totalmax)).flatten().tolist()]
-	totalmaxdatehours = [d for n, d in enumerate(totalmaxdatehours) if d not in totalmaxdatehours[:n]]
+	totalmaxtimestamps = sorted([totaldata['timestamp'][i] for i in (numpy.argwhere(totaldata['value'] == totalmax)).flatten().tolist()])
 	
-	if verbose:
-		out = "Maximum: \t" + str(totalmax) + " ("
-		for i in range(0,len(totalmaxdatehours)):
-			out = out + totalmaxdatehours[i]
-			if i < len(totalmaxdatehours)-1:
-				out = out + ", "
-		out = out + ")"
-		print out
+	totalmaxdatetimes = []
+	totalmaxdatehours = []
+	for t in totalmaxtimestamps:
+		dbcursor.execute("Select Year,Month,Day,Hour,Minute,Second FROM Data WHERE Timestamp IS "+str(t)+" AND Sensor IS "+str(sensor))
+		res = dbcursor.fetchone()
+		s = str(res[0])+"-"+str(res[1]).zfill(2)+"-"+str(res[2]).zfill(2)+" "+str(res[3]).zfill(2)+":"+str(res[4]).zfill(2)+":"+str(res[5]).zfill(2)
+		totalmaxdatetimes.append(s)
+		sh = DateHelper.DateHourFromDatetime(s)
+		if not sh in totalmaxdatehours:
+			totalmaxdatehours.append(sh)
+	
+	out = "  Maximum: \t" + str(totalmax) + " ("
+	for i in range(0,len(totalmaxdatehours)):
+		out = out + totalmaxdatehours[i]
+		if i < len(totalmaxdatehours)-1:
+			out = out + ", "
+	out = out + ")"
+	print out
 	
 	#total minimum
-	totalmin = totaldata['value'].min()
-	totalmindatehours = [DateAndHourFromTimestamp(totaldata['timestamp'][i]) for i in (numpy.argwhere(totaldata['value'] == totalmin)).flatten().tolist()]
-	totalmindatehours = [d for n, d in enumerate(totalmindatehours) if d not in totalmindatehours[:n]]
+	#total maximum
+	totalmin = totaldata['value'].min()	
+	totalmintimestamps = sorted([totaldata['timestamp'][i] for i in (numpy.argwhere(totaldata['value'] == totalmin)).flatten().tolist()])
 	
-	if verbose:
-		out = "Minimum: \t" + str(totalmin) + " ("
-		for i in range(0,len(totalmindatehours)):
-			out = out + totalmindatehours[i]
-			if i < len(totalmindatehours)-1:
-				out = out + ", "
-		out = out + ")"
-		print out
+	totalmindatetimes = []
+	totalmindatehours = []
+	for t in totalmintimestamps:
+		dbcursor.execute("Select Year,Month,Day,Hour,Minute,Second FROM Data WHERE Timestamp IS "+str(t)+" AND Sensor IS "+str(sensor))
+		res = dbcursor.fetchone()
+		s = str(res[0])+"-"+str(res[1]).zfill(2)+"-"+str(res[2]).zfill(2)+" "+str(res[3]).zfill(2)+":"+str(res[4]).zfill(2)+":"+str(res[5]).zfill(2)
+		totalmindatetimes.append(s)
+		sh = DateHelper.DateHourFromDatetime(s)
+		if not sh in totalmindatehours:
+			totalmindatehours.append(sh)
 	
+	out = "  Minimum: \t" + str(totalmin) + " ("
+	for i in range(0,len(totalmindatehours)):
+		out = out + totalmindatehours[i]
+		if i < len(totalmindatehours)-1:
+			out = out + ", "
+	out = out + ")"
+	print out
+		
 	#total average
 	totalavg = totaldata['value'].mean()
 	totalsigma = totaldata['value'].std()
-	if verbose:
-		print "Average:\t" + str(round(totalavg,3)) + " (σ=" + str(round(totalsigma,3))+")"
+	print "  Average:\t" + str(round(totalavg,3)) + " (σ=" + str(round(totalsigma,3))+")"
+	
+	#daily data
+	dates = list(set([ (d[0],d[1],d[2]) for d in data.keys() ]))
+	hoursfordate = dict()
+	for date in dates:
+		hoursfordate = sorted([ d[3] for d in data.keys() if d[0] == date[0] and d[1] == date[1] and d[2] == date[2] ])
 		
-	#daily maximum
-	dailymax = numpy.array([ (time.strftime("%Y-%m-%d", day + [0,0,0,0,0,0]), numpy.concatenate([ data[day[0]][day[1]][day[2]][h] for h in data[day[0]][day[1]][day[2]].keys() ])['value'].max()) for day in availabledates ], dtype=[('date', '|S10'),('value',numpy.float64)])	
-	dailymaxaverage = dailymax['value'].mean()
+	dailydata = dict()
+	for date in dates:
+		dailydata[date] = numpy.concatenate([data[d] for d in data.keys() if d[0] == date[0] and d[1] == date[1] and d[2] == date[2]])
+		
+	dailymax = numpy.array([ (date, dailydata[date]['value'].max()) for date in dates], dtype=[('date', (numpy.int32,(1,3))), ('value',numpy.float64)])
+	dailymaxavg = dailymax['value'].mean()
 	dailymaxsigma = dailymax['value'].std()
-	if verbose:
-		print "Daily maximum:\t" + str(round(dailymaxaverage,3)) + " (σ=" + str(round(dailymaxsigma,3))+")"	
+	print "  Daily max:\t" + str(round(dailymaxavg,3)) + " (σ=" + str(round(dailymaxsigma,3))+")"	
 	
-	#daily minimum
-	dailymin = numpy.array([ (time.strftime("%Y-%m-%d", day + [0,0,0,0,0,0]), numpy.concatenate([ data[day[0]][day[1]][day[2]][h] for h in data[day[0]][day[1]][day[2]].keys() ])['value'].min()) for day in availabledates ], dtype=[('date', '|S10'),('value',numpy.float64)])	
-	dailyminaverage = dailymin['value'].mean()
+	dailymin = numpy.array([ (date, dailydata[date]['value'].min()) for date in dates], dtype=[('date', (numpy.int32,(1,3))), ('value',numpy.float64)])
+	dailyminavg = dailymin['value'].mean()
 	dailyminsigma = dailymin['value'].std()
-	if verbose:
-		print "Daily minimum:\t" + str(round(dailyminaverage,3)) + " (σ=" + str(round(dailyminsigma,3))+")"	
-		
-	#hour data
-	hourdata = dict()
-	for h in range(0,24):
-		hdata = [ data[e[0]][e[1]][e[2]][e[3]] for e in availabledatehours if e[3] == h ]
-		if len(hdata) == 0:
-			continue
-		else:
-			hourdata[h] = numpy.concatenate(hdata)
+	print "  Daily min:\t" + str(round(dailyminavg,3)) + " (σ=" + str(round(dailyminsigma,3))+")"	
 	
-	#hour average
-	availablehours = hourdata.keys()
-	houravg = [ hourdata[h]['value'].mean() for h in availablehours ]
-	hourstd = [ hourdata[h]['value'].std() for h in availablehours ]
-			
-	#hour average plot
-	#plt.errorbar(availablehours, houravg, hourstd, marker='^')
-	#plt.xlim([min(availablehours)-1,max(availablehours)+1])
-	#plt.xlabel('Hour')
-	#plt.ylabel('Average ' + measurand + " (" + unit + ")")
-	#plt.xticks(availablehours, availablehours)
-	#plt.figure()
 	
 	#month data
-	monthdata = dict()
-	for m in range(1,13):
-		mdata = [ data[e[0]][e[1]][e[2]][e[3]] for e in availabledatehours if e[1] == m ]
-		if len(mdata) == 0:
-			continue
-		else:
-			monthdata[m] = numpy.concatenate(mdata)
+#	monthdata = dict()
+#	for m in range(1,13):
+#		mdata = [ data[e[0]][e[1]][e[2]][e[3]] for e in availabledatehours if e[1] == m ]
+#		if len(mdata) == 0:
+#			continue
+#		else:
+#			monthdata[m] = numpy.concatenate(mdata)
 			
 	#month average
-	availablemonths = monthdata.keys()
-	monthavg = [ monthdata[h]['value'].mean() for h in availablemonths ]
-	monthstd = [ monthdata[h]['value'].std() for h in availablemonths ]
+#	availablemonths = monthdata.keys()
+#	monthavg = [ monthdata[h]['value'].mean() for h in availablemonths ]
+#	monthstd = [ monthdata[h]['value'].std() for h in availablemonths ]
 			
 	#hour average plot
 	#if len(availablemonths) > 1:
@@ -441,16 +392,8 @@ for sensor in sensors:
 	res = ReadData(sensor,years, months, days, hours, start, end)
 	datehours = res[0]
 	data = res[1]
-	quality = res[2]
-	
-	sys.exit(0)
-	
-	tmp = GetSensorQuality(sensor, datehours,verbose=True)
-	quality = round(tmp[0],3)
-	missingdatehours = tmp[1]
-	numdatapoints = tmp[2]
-		
-	Analyze(sensor, datehours,verbose=True)
+	quality = res[2]		
+	Analyze(sensor, data)
 	
 	if len(sensors) > 1:
 		print ""
